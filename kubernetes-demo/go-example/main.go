b77f48b01f597771953073cd13b03d43bf3310e7
Albert Li <albertli88@gmail.com>
2018-10-24 16:54:22 -0700

    [SWDEV-64] keep track and print all pods running in minikube

--- kubernetes-demo/go-example/main.go
+++ kubernetes-demo/go-example/main.go
@@ -21,7 +21,7 @@ import (
 	"fmt"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
+	// "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -31,33 +31,41 @@ func main() {
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
+		fmt.Printf("Yikes, panic -1\n")
 		panic(err.Error())
 	}
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
+		fmt.Printf("Yikes, panic 0\n")
 		panic(err.Error())
 	}
 	for {
 		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
 		if err != nil {
+			fmt.Printf("Yikes, panic 1\n")
 			panic(err.Error())
 		}
 		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
+		for i := 0; i < len(pods.Items); i++ {
+			fmt.Printf("There are %s pods in the cluster\n", pods.Items[i])
+		}
+
 		// Examples for error handling:
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("default").Get("hello-node", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod not found\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod\n")
-		}
+		// _, err = clientset.CoreV1().Pods("default").Get("hello-node", metav1.GetOptions{})
+		// if errors.IsNotFound(err) {
+		// 	fmt.Printf("Pod not found\n")
+		// } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		// 	fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		// } else if err != nil {
+		// 	fmt.Printf("Yikes, panic 2\n")
+		// 	panic(err.Error())
+		// } else {
+		// 	fmt.Printf("Found pod\n")
+		// }
 
 		time.Sleep(10 * time.Second)
 	}
