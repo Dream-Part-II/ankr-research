2b5c0b5916799ba75a5ca500395b97aabbf994d1
msaxena18 <msaxena@berkeley.edu>
2018-11-06 22:03:34 -0800

    [SWDEV-69] Added Resource Calculation Function

--- microraiden/examples/microraiden_basic_demo_ankr.py
+++ microraiden/examples/microraiden_basic_demo_ankr.py
@@ -6,21 +6,28 @@ In this file, we construct a channel between two parties, a sender and a recevie
 from microraiden import Client
 import os
 
+
+"""Calculates Price of Resources Utilized """
+def resource_calculation():
+    return 1 # replace with resource calculation logic
+
 receiver = '0xaaaaaaaaaaaaaaaaaaaaaaa'
 privkey = '0xaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' # Some sender private key
+ 
 
 client = Client(privkey)
+resource_price = resource_calculation()
 # Open a suitable channel for communication and deposit 1 TKN
-channel = client.get_suitable_channel(receiver, 1)
-channel.create_transfer(1)
+channel = client.get_suitable_channel(receiver, resource_price)
+channel.create_transfer(resource_price)
 
 print(
     'Current balance proof:\n'
     'From: {}\n'
     'To: {}\n'
     'Channel opened at block: #{}\n'  # used to uniquely identify this channel
-    'Balance: {}\n'                   # total: 0
-    'Signature: {}\n'                 # valid signature for a balance of 0 on this channel
+    'Balance: {}\n'                   # total: 1
+    'Signature: {}\n'                 # valid signature for a balance of 1 on this channel
     .format(
         channel.sender, channel.receiver, channel.block, channel.balance, channel.balance_sig
     )
