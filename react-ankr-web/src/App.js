4639147e400fc21dc2f14bfbf5faf74326076239
Xiaoping-Li <narci2008@gmail.com>
2018-12-21 11:53:08 -0800

    [SWDEV-120]Initial components

--- react-ankr-web/src/App.js
+++ react-ankr-web/src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
+import Home from './components/Home';
+import About from './components/About';
+import Product from './components/Product';
+import Contacts from './components/Contacts';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello world!
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/product' component={Product} />
+          <Route path='/contacts' component={Contacts} />
+        </div>
+      </BrowserRouter> 
     );
   }
 }
