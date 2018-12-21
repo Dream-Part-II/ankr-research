44d7db3828ec958f7735b738388ec0c805343f65
Xiaoping-Li <narci2008@gmail.com>
2018-12-21 12:17:14 -0800

    [SWDEV-120]Basic layout of project

---
+++ react-ankr-web/src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class Navigation extends Component {
+    render() {
+        return(
+            <div className='nav'>
+                <Link key={1} to=''>Home</Link>
+                <Link key={2} to='/about'>About</Link>
+                <Link key={3} to='/product'>Product</Link>
+                <Link key={4} to='/contacts'>Contacts</Link>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
