74db5fdc44777c1d305368a721d7f09b0a132bc5
Xiaoping-Li <narci2008@gmail.com>
2018-12-21 16:34:00 -0800

    [SWDEV-120]Modify About folder

---
+++ react-ankr-web/src/components/about/ChangeWorld.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class ChangeWorld extends Component {
+    render() {
+        return (
+            <div>
+                <h2>We Dare to Change the World</h2>
+                <p>Building a better cloud is never an easy task, but we believe we are paving the way for 
+                    a more open cloud ecosystem where businesses and consumers across the world can interconnect 
+                    and benefit.</p>
+
+                <div>
+                    <div>
+                        {/*<img src="" alt="" /> */}
+                        <h4>Build</h4>
+                        <div>Build a resource-efficient blockchain framework</div>
+                    </div>
+                    <div>
+                        {/*<img src="" alt="" /> */}
+                        <h4>Connect</h4>
+                        <div>Connect the computing power across the world</div>
+                    </div>
+                    <div>
+                        {/*<img src="" alt="" /> */}
+                        <h4>Evolve</h4>
+                        <div>Continue improving and pushing our limits</div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ChangeWorld;
