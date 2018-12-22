77c9e71169693d51fd98fc6469da984469adae11
GeorgeBian <bianz20@berkeley.edu>
2018-12-21 16:00:54 -0800

    [SWDEV-120] home page main updated

--- react-ankr-web/src/components/Home.js
+++ react-ankr-web/src/components/Home.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-
+import Main from './home/main';
 
 class Home extends Component {
     render() {
         return(
             <div>
                 Welcome to Home Page!
+                <div> 
+                    <Main /> 
+                </div>
             </div>
+            
+            
         );
     }
 }
