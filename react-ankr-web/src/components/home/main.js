cc549a378bf4474189069d65dd7591dcf8972b2d
GeorgeBian <bianz20@berkeley.edu>
2018-12-21 16:00:54 -0800

    [SWDEV-120] home page main updated

---
+++ react-ankr-web/src/components/home/main.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class Main extends Component {
+    render() {
+        return ( 
+        <div>
+            <p>
+            Build a Faster, Cheaper, Securer cloud using idle processing power 
+            in data centers and edge devices
+            </p>
+            <div>
+
+            </div>
+        </div>
+        );
+    }
+}
+
+export default Main;
