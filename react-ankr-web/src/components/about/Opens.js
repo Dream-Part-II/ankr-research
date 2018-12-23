f84a9d67cd52c3c8dcdb49e5187bc58a7abbaa69
Xiaoping-Li <narci2008@gmail.com>
2018-12-23 13:50:19 -0800

    [SWDEV-129]Add Opens to About

--- react-ankr-web/src/components/about/Opens.js
+++ react-ankr-web/src/components/about/Opens.js
@@ -1,1 +1,30 @@
 import React, { Component } from 'react';
+import { Opennings } from "../../data/opennings";
+
+class Opens extends Component {
+    constructor() {
+        super();
+        this.state = {
+            opens: Opennings,
+        };
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.opens.map((open, idx) => {
+                    return (
+                        <li key={idx}>
+                            {open.title}
+                            {open.type}
+                            {open.location}
+                            <button>SEND REQUEST</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        );
+    }
+}
+
+export default Opens;
---
+++ react-ankr-web/src/data/opennings.js
@@ -0,0 +1,22 @@
+export const Opennings = [
+    {
+        title: 'Product Manager',
+        type: 'Product Manager',
+        location: 'San Francisco, CA'
+    },
+    {
+        title: 'Business Development Assistant',
+        type: 'Part Time',
+        location: 'San Francisco, CA'
+    },
+    {
+        title: 'Front-end Developer',
+        type: 'Internship',
+        location: 'San Francisco, CA'
+    },
+    {
+        title: 'Software Engineer',
+        type: 'Full Time',
+        location: 'Shanghai, China'
+    }
+];
