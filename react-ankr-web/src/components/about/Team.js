ecafcaa1968de2c449a2f83a410158f1a6696225
Xiaoping-Li <narci2008@gmail.com>
2018-12-22 19:21:14 -0800

    [SWDEV-120]Add team info and images folder

--- react-ankr-web/src/components/about/Team.js
+++ react-ankr-web/src/components/about/Team.js
@@ -1,1 +1,18 @@
 import React, { Component } from 'react';
+
+import Staff from '../../data/staff';
+
+class Team extends Component {
+    constructor() {
+        super();
+        this.state = {
+            staff: Staff
+        };
+    }
+
+    render() {
+
+    }
+}
+
+export default Team;
---
+++ react-ankr-web/src/data/staff.js
@@ -0,0 +1,114 @@
+import albertImg from '../images/albert.jpg';
+import andersonImg from '../images/anderson.jpg';
+import chandlerImg from '../images/chandler-1.jpg';
+import giacomoImg from '../images/giacomo.jpg';
+import hanpinImg from '../images/hanpin.jpg';
+import milanImg from '../images/milan.jpg';
+import quanImg from '../images/quan.jpg';
+import ramseyImg from '../images/ramsey.jpg';
+import ryanImg from '../images/ryan.jpg';
+import ssongImg from '../images/ssong.jpg';
+import stanleyImg from '../images/stanly.jpg';
+import wenliImg from '../images/wenli.jpg';
+import yifanImg from '../images/yifan.jpg';
+import zhangImg from '../images/zhang.jpg';
+
+
+
+export const Staff = [
+    {
+        name: 'Chandler Song',
+        title: 'Co-founder & CEO',
+        description: 'Chandler Song is an Engineer-Turned-Entrepreneur. He worked at Amazon Ads Team, Amazon Lab126 in IoT and FireOS as well as SAP Enterprise Supply Management. Chandler invested early in Bitcoin and was involved with the blockchain student organization during college at UC Berkeley. He is also passionate about product and UI/UX design and had lead the Wechat Mini-Program development for CitySpade, a P2P real estate brokerage startup in New York.',
+        img: chandlerImg,
+    },
+    {
+        name: 'Stanley Wu',
+        title: 'Co-founder & CTO',
+        description: 'Stanley spent his last 10 years at Amazon as a Tech Lead prior to founding Ankr. He was among the early engineers to join Amazon Web Service in 2008 and has been a core member of Alexa Internet. He is extremely knowledgeable in large-scale distributed systems, search engine, browser tech, full stack development.',
+        img: stanleyImg,
+    },
+    {
+        name: 'Ryan Fang',
+        title: 'Co-founder & COO',
+        description: 'Ryan Fang quit Morgan Stanley Investment Banking Division to found Ankr. Prior, he worked at Credit Suisse and China Renaissance as an Investment Bank Analyst. He had previously sold his company, Peer Potential Mock Trial to an education company in China.',
+        img: ryanImg,
+    },
+    {
+        name: 'David P. Anderson',
+        title: 'Technical Advisor',
+        description: 'David P. Anderson is the creator of BOINC (Berkeley Open Infrastructure for Network Computing), the largest volunteer-based distributed computing platform in the world with over 200,000 monthly active users. He is also a research scientist at the Space Science Laboratory at Berkeley and leads the famous SETI@home project. He received the NSF Presidential Young Investigator Award and is an author and inventor of multiple programming languages and operating systems.',
+        img: andersonImg,
+
+    },
+    {
+        name: 'Song Liu',
+        title: 'Chief Security Engineer',
+        description: 'Song Liu is a network security expert, having served as the Principal Engineer at Gigamon and Palo Alto Networks for over 15 years combined. He himself is an ethical hacker and has presented various security vulnerabilities to large corporations including Microsoft.',
+        img: ssongImg,
+
+    },
+    {
+        name: 'Giacomo Ghidini',
+        title: 'Chief Scientist',
+        description: 'Giacomo Ghidini has solid experiences in cellular network, distributed systems and cloud computing. He received his Ph.D. degree from University of Texas Arlington and had since advised graduate students as an Adjunct Professor. He has worked at Siemens, Oracle and Numerix as a researcher. In 2012, he received an NSF grant for his project in energy-efficient protocols for smart devices.',
+        img: giacomoImg,
+
+    },
+    {
+        name: 'John Zhang',
+        title: 'Senior Software Engineer',
+        description: 'John Zhang is an expert in C++ and web development. He has over 13 years of developing products for early stage startups.',
+        img: zhangImg,
+
+    },
+    {
+        name: 'Ramsey Hanna',
+        title: 'Legal & Compliance Advisor',
+        description: 'Ramsey Hanna is a partner at Reed Smith, LLP. He obtained his J.D. from Stanford University and has been specialized in corporate law for over 20 years.',
+        img: ramseyImg,
+
+    },
+    {
+        name: 'Hanpin Lin',
+        title: 'Blockchain Engineer',
+        description: 'Hanping Lin is a Software Engineer with a demonstrated history of cloud computing experience in the information technology and services industry. Skilled in Storage, IT Service Management, Data Center, and Virtualization. Strong software engineering professional graduated from San Jose State University.',
+        img: hanpinImg,
+
+    },
+    {
+        name: 'Albert Li',
+        title: 'Software Engineer Intern',
+        description: 'He is a 4th year Computer Science major at UCLA. He has explored working with many different technologies, ranging from low level from machine code and C to web development.',
+        img: albertImg,
+
+    },
+    {
+        name: 'Milan Saxena',
+        title: 'Blockchain Engineer Intern',
+        description: 'He is currently senior at UC Berkeley studying Computer Science. These past four years he has had a plethora of work experiences in software development, data science, and analytics. He has worked at Memorial Sloan Kettering Cancer Center and Infor.',
+        img: milanImg,
+
+    },
+    {
+        name: 'Yifan Zhang',
+        title: 'UI/UX designer',
+        description: 'Yifan Zhang is a UI/UX designer, specializing in the web/app design & visual effect. She was trained at the Academy of Arts University. Her previous design work experience includes developing visual design at Ylabs, conducting UI/UX design and branding strategies at Young & Hungry Agency, supporting visual design for Plenry.',
+        img: yifanImg,
+
+    },
+    {
+        name: 'Wenli Zheng',
+        title: 'Researcher',
+        description: 'Wenli Zheng is an assistant Professor at Shanghai Jiao Tong University, Research Assistant at Ohio State University, Expert in computer architecture and data center infrastructure.',
+        img: wenliImg,
+
+    },
+    {
+        name: 'Quan Chen',
+        title: 'Researcher',
+        description: 'Quan Chen is an assistant Professor at Shanghai Jiao Tong University, Post-doc Researcher at Columbia University and University of Michigan Ann Arbor, Expert in distributed systems.',
+        img: quanImg,
+
+    },
+];
