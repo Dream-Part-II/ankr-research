a2300a5fa5f5d9ccf1f85f803dc0de04d1dd7927
Xiaoping-Li <narci2008@gmail.com>
2018-12-23 15:55:52 -0800

    [SWDEV-120]Add Footer component, could be shared by all main components

---
+++ react-ankr-web/src/components/Footer.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {Button, FormControl, FormGroup} from "react-bootstrap";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                {/*Add TeamForm and PartnerForm here, followed by "stay_tuned"*/}
+
+                {/*"stay_tuned" below*/}
+                <div className="stay_tuned">
+                    <div className="left">
+                        <h2>Stay tuned</h2>
+                        <div>Stay tuned for the latest news</div>
+
+                        <form>
+                            <FormGroup>
+                                <FormControl type="text" placeholder="Your email address" />
+                            </FormGroup>
+
+                            <Button type="submit">Subscribe</Button>
+                        </form>
+
+                        <div>support@ankr.network</div>
+                    </div>
+
+                    <div className="right">
+                        <div className="about_footer">
+                            <h5>About</h5>
+                            <ul>
+                                <li>STORY</li>
+                                <li>CAREER</li>
+                                <li>TEAM</li>
+                                <li>PRODUCT</li>
+                                <li>NEWS</li>
+                            </ul>
+                        </div>
+                        <div className="support_footer">
+                            <h5>Support</h5>
+                            <ul>
+                                <li>FAQ</li>
+                                <li>CONTACT</li>
+                                <li>LOGO ASSETS</li>
+                                <li>WHITE PAPER</li>
+                                <li>TECHNOLOGY</li>
+                            </ul>
+                        </div>
+                        <div className="social_footer">
+                            <h5>Social</h5>
+                            <ul>
+                                <li>Medium</li>
+                                <li>Twitter</li>
+                                <li>Telegram</li>
+                                <li>Youtube</li>
+                                <li>Wechat</li>
+                                <li>Instagram</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+                <footer>
+                    <p>&copy; 2018 Ankr All rights reserved</p>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
