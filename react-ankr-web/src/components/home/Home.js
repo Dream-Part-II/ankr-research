7b9d785193090a60201328c069273bee814a10a5
George Bian <bianz20@berkeley.edu>
2018-12-28 11:03:42 -0800

    [SWDEV-120] basic structure for home page

--- react-ankr-web/src/components/home/Home.js
+++ react-ankr-web/src/components/home/Home.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import Main from './main';
+import WhatsAnkr from './whats_ankr';
+import WhyAnkr from './why_ankr';
+import UseCases from './use_cases';
+import RoadMap from './road_map';
+import Partners from './partners';
+import LatestNews from './latest_news';
 
 class Home extends Component {
     render() {
@@ -8,6 +14,12 @@ class Home extends Component {
                 Welcome to Home Page!
                 <div> 
                     <Main /> 
+                    <WhatsAnkr />
+                    <WhyAnkr />
+                    <UseCases />
+                    <RoadMap />
+                    <Partners />
+                    <LatestNews />
                 </div>
             </div>
             
--- react-ankr-web/src/components/home/main.js
+++ react-ankr-web/src/components/home/main.js
@@ -9,8 +9,10 @@ class Main extends Component {
             Build a Faster, Cheaper, Securer cloud using idle processing power 
             in data centers and edge devices
             </p>
-            <div>
-
+            <div>   
+                <button> 
+                    request a demo   
+                </button>
             </div>
         </div>
         );
