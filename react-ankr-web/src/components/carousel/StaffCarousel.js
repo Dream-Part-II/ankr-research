81f1d8972cf666ded8c2b48a5c6885354d4fe9ae
Xiaoping-Li <narci2008@gmail.com>
2019-01-02 13:14:14 -0800

    [SWDEV-120]StaffCarousel functions well with staff list

--- react-ankr-web/src/components/carousel/StaffCarousel.js
+++ react-ankr-web/src/components/carousel/StaffCarousel.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import { Carousel, Glyphicon } from 'react-bootstrap';
+import { Staff } from '../../data/staff';
+import './StaffCarousel.css';
+
 
 class StaffCarousel extends Component {
 
+    render() {
+        return (
+            <div>
+                <Carousel
+                    interval={null}
+                    defaultActiveIndex={this.props.id}
+                >
+                    {Staff.map((item, idx) =>
+                        <Carousel.Item key={idx}>
+                            <img src={item.img}/>
+                            <Carousel.Caption>
+                                <div>{item.name}</div>
+                                <div>{item.title}</div>
+                                <p>{item.description}</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>)}
+                </Carousel>
+            </div>
+        );
+    }
 }
 
 export default StaffCarousel;
