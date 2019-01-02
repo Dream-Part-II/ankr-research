2193a117041d1c8ff90ab4777cbe37bfac203626
Xiaoping-Li <narci2008@gmail.com>
2019-01-02 11:17:35 -0800

    [SWDEV-120]Modify faq.js

--- react-ankr-web/src/components/contacts/Contacts.js
+++ react-ankr-web/src/components/contacts/Contacts.js
@@ -11,19 +11,24 @@ class Contacts extends Component {
                 <div>
                     <h2>Contacts</h2>
                     <div className="us">
+                        <span className="fas fa-map-marker-alt"></span>
                         <div>San Francisco Office</div>
                         <div>609 Mission St 2nd floor</div>
                         <div>San Francisco, CA 94105</div>
                     </div>
 
                     <div className="china">
+                        <span className="fas fa-map-marker-alt"></span>
                         <div>Shanghai Office</div>
                         <div>Room 1108, Neo Bay Technology Building,</div>
                         <div>951 Jianchuan Road, District of Minghang</div>
                         <div>1108室零号湾科技大楼（闵行区剑川路951号)</div>
                     </div>
 
-                    <div>support@ankr.network</div>
+                    <div>
+                        <span className="far fa-envelope"></span>
+                        support@ankr.network
+                    </div>
                 </div>
 
                 {/*2nd part: "Contact form"*/}
