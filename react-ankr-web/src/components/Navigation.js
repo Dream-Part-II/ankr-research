bead4108faf36da7431d48598348e4d3b2af9a29
Xiaoping-Li <narci2008@gmail.com>
2019-01-03 14:07:19 -0800

    [SWDEV-120]Add PartnerForm to Navbar

--- react-ankr-web/src/components/Navigation.js
+++ react-ankr-web/src/components/Navigation.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+
 import TeamForm from './forms/TeamForm';
+import PartnerForm from './forms/PartnerForm';
+import PartnerFrom from "./Footer";
 
 
 class Navigation extends Component {
@@ -13,7 +16,7 @@ class Navigation extends Component {
                 <Link key={3} to='/product'>Product</Link>
                 <Link key={4} to='/contacts'>Contacts</Link>
                 <TeamForm show={this.props.teamShow} onChange={this.props.onTeamChange} />
-
+                <PartnerForm show={this.props.partnerShow} onChange={this.props.onPartnerChange} />
                 <Button>REQUEST A DEMO</Button>
 
             </div>
