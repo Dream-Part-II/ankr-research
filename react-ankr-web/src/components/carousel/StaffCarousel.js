bbc8bf478b53318293cd7965b34aa4352644a690
Xiaoping-Li <narci2008@gmail.com>
2019-01-08 16:24:20 -0800

    [SWDEV-120]Modify staff carousel style

--- react-ankr-web/src/components/carousel/StaffCarousel.js
+++ react-ankr-web/src/components/carousel/StaffCarousel.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Carousel, Modal } from 'react-bootstrap';
 import { Staff } from '../../data/staff';
-import './StaffCarousel.css';
+import '../../css/StaffCarousel.css';
 
 
 class StaffCarousel extends Component {
@@ -9,22 +9,36 @@ class StaffCarousel extends Component {
     render() {
         return (
             <div>
-                <Modal show={this.props.show} onHide={this.props.onChange}>
+                <Modal
+                    show={this.props.show}
+                    onHide={this.props.onChange}
+                    dialogClassName="staff-carousel-modal"
+                >
                     <Modal.Header closeButton />
 
                     <Modal.Body>
                         <Carousel
                             interval={null}
                             defaultActiveIndex={this.props.id}
+                            className="staff-carousel"
                         >
                             {Staff.map((item, idx) =>
-                                <Carousel.Item key={idx}>
-                                    <img src={item.img} alt="photos"/>
-                                    <Carousel.Caption>
-                                        <div>{item.name}</div>
-                                        <div>{item.title}</div>
-                                        <p>{item.description}</p>
-                                    </Carousel.Caption>
+                                <Carousel.Item
+                                    key={idx}
+                                >
+                                    <div className="staff-carousel-item">
+                                        <img
+                                            src={item.img} alt="photos"
+                                            className="staff-carousel-item-img"
+                                        />
+
+                                        <div className="staff-carousel-item-info">
+                                            <div className="staff-carousel-item-name">{item.name}</div>
+                                            <div className="staff-carousel-item-title">{item.title}</div>
+                                            <div className="staff-carousel-item-divider"></div>
+                                            <p className="staff-carousel-item-desc">{item.description}</p>
+                                        </div>
+                                    </div>
                                 </Carousel.Item>)}
                         </Carousel>
                     </Modal.Body>
